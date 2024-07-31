3feb0477c5a705978015cddd8733c058b26881b4
TranPhong09 <tranphuocphong565@gmail.com>
2024-07-31 10:51:24 -0400

    first commit

---
+++ setting.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define lls unsigned long long 
+#define ll long long
+#define GCD(a, b) __gcd(a,b) 
+#define LCM(a, b) (a*b)/__gcd(a,b) 
+#define ii pair<int,int> 
+#define fs first
+#define sc second 
+#define FOR(i, a, b, c) for(int i = a; i <= b ; i += c)
+#define maxn 100001 
+#define nn "\n"
+using namespace std;
+
+int main() {
+    freopen("TEST.INP", "r", stdin);
+    freopen("TEST.OUT", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    return 0;
+}
